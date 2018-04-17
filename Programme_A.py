4093a9f4c676f8daa5592d96581d7879c81958cb
Shannon Ormond <shamormo@gmail.com>
2018-04-17 20:53:10 +1200

    Testing out uploading of the script

---
+++ Programme_A.py
@@ -0,0 +1,7 @@
+fastq_ori = open("/Users/sormond/Dropbox/2018/Computational Biology/Progamming assignment 1/TestFiles/inputtest.txt", "r")
+
+fastq_list = fastq_ori.readlines()
+
+for number in 0:39 :
+    if number != 0, 1, 0+(4, 8, 16), 1+(4, 8, 16)
+        print fastq_list[number]
