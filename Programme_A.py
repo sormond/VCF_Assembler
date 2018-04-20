5c72218e2e47c949155d39e346a7430ad09d68d9
Shannon Ormond <sormond@it072691.massey.ac.nz>
2018-04-20 17:38:54 +1200

    more comments throughout and more code added to VCF file compiler. Compiler now functional but need to improve and add comments. VCF compiler adds previously added variants, this needs fixing.

--- Programme_A.py
+++ Programme_A.py
@@ -1,8 +1,8 @@
 wkdirectory = "/Users/sormond/Desktop"
 reference_file = "reference.fna"
-fastq_file = "test.fq"
-fasta_file = "test.fa"
-vcf_file = "name of output vcf file"
+fastq_file = "input.fq"
+fasta_file = "output.fa"
+vcf_file = "outputvcf.vcf"
 
 import os
 os.chdir(wkdirectory)
@@ -13,7 +13,7 @@ with open(reference_file, 'r') as r:
 with open(fastq_file, 'r') as f:
     fastq_list = f.readlines()
 
-# function which breaks list elements into chunks (list of lists) of a chosen size
+# function which breaks list items into chunks (list of lists) of a chosen size
 def chunks(l, n):
     """Yield successive n-sized chunks from l"""
     newlist = []
@@ -35,37 +35,57 @@ for i in range(1, len(fqn), 2) :
 
 print(len(fqn))  # check, len should = 10
 
+# extract all items from lists wthin lists of 'fqn' and create list, 'fqn2'
 fqn2 = []
 for i in range(0, len(fqn)) :
     fqn2.append(fqn[i][0])
     fqn2.append(fqn[i][1])
 
-print(fqn2)
-print(len(fqn2))
+print(fqn2) # just a check
+print(len(fqn2)) # just a check
 
+# write new fasta file
 fastafile = open(fasta_file, 'w')
-# fastafile.write(str(fqn))
 for item in fqn2:
-  fastafile.write(item)
+    # for each item of fqn2, write new line in fasta file
+    fastafile.write(item)
 fastafile.close()
-# must close connection to file:    outfile.close()
+
 
 """ VCF file compiler """
 
 # perform 'chunks' on 'fastq_list' to break the fastq file into chunks of 4 lines
 new = chunks(fastq_list, 4)
 
-# add code for headers for outfile here
+# creates vcf file with headers
+headers = ["#CHROM\t", "POS\t", "ID\t", "REF\t", "ALT\t", "QUAL\t", "FILTER\t", "INFO\t"]
+vcffile = open(vcf_file, 'w')
+for item in headers:
+    vcffile.write(item)
 
 # with open("filename.txt", "w") as outfile:
-for i in range(0, 1) :
+for i in range(0, len(new)) :
     a = new[i][1]
     for j in range(0, len(a)) :
         if a[j] == ref[j] :
-            print("true" + (a[j]) + ref[j])
+            pass
+            # print("true" + (a[j]) + ref[j])
         else :
-            print("false" + a[j] + ref[j])
-            # outfile.write(
+            POS = str(j)
+            ID = str(new[i][0])
+            ID = ID.rstrip()  # removes '/n' from ID string
+            REF = a[j]
+            ALT = ref[j]
+            vcffile.write("\n")
+            vcffile.write("1\t")
+            vcffile.write("%s\t" % POS)
+            vcffile.write("%s\t" % ID)
+            vcffile.write("%s\t" % REF)
+            vcffile.write("%s\t" % ALT)
+            # print("false" + a[j] + ref[j])
+vcffile.close()
+
+# need to stop variants being added to vcf file if they already exist in vcf file
 
 ## \t = tab (use for making vcf file)
 ## writing things to file : outfile = open('filename, 'w')
