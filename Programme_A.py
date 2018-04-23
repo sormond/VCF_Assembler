9c9160b8bdda14ac64c06524607636412b8c7376
Shannon Ormond <sormond@it072691.massey.ac.nz>
2018-04-23 15:40:27 +1200

    Added comments, added script which removes '@' at beginning of read ID and replaces with '>' for proper conversion to fasta file format

--- Programme_A.py
+++ Programme_A.py
@@ -1,40 +1,40 @@
-import argparse
+#!/usr/bin/env python3
 
+import argparse
 parser = argparse.ArgumentParser()
 parser.add_argument('fastq', help = "fastq file to be processed")
 parser.add_argument('-V', '--VCF', help = "If reference.fasta passed, creates a VCF file. Otherwise, fasta will be generated from fastq file.")
 
 args = parser.parse_args()
-reference_file = ""
-if args.VCF :
-    reference_file = args.vcf
+reference_file = "" # defines reference_file
+if args.VCF : # if a VCF file given, loads file into 'reference_file
+    reference_file = args.VCF
 
-# wkdirectory = "C:/Users\Shannon\Desktop\Programming_Assignment_A\TestFiles1" # add working directory here
-# reference_file = "reference.fna" # add reference sequence here
 fastq_file = args.fastq  # add fastq file here
-# fasta_file = "output.fa" # specify output fasta file name here
-# vcf_file = "outputvcf.vcf" # specify output VCF file name here
-
-# opens reference file and extracts line containing sequence and inserts it into a list, 'ref'
-with open(reference_file, 'r') as r:
-    r = r.readlines()
-    ref = r[1]
+fasta_file = "output.fa"
+vcf_file = "outputvcf.vcf"
 
 # opens fastq file
 with open(fastq_file, 'r') as f:
     fastq_list = f.readlines()
 
-# function which breaks list items into chunks (list of lists) of a chosen size
+# function which breaks list items into chunks (list of lists) of a chosen size (modified from code retrieved from "https://stackoverflow.com/questions/312443/how-do-you-split-a-list-into-evenly-sized-chunks")
 def chunks(l, n):
-    """Yield successive n-sized chunks from l"""
+    # Yield successive n-sized chunks from l
     newlist = []
     for i in range(0, len(l), n) :
         newlist.append(l[i:i + n])
     return newlist
 
-""" VCF file compiler """
+
+    """ VCF file compiler """
 
 if args.VCF :
+    #  extracts line containing sequence from 'reference_file' and inserts it into a list, 'ref'
+    with open(reference_file, 'r') as r:
+        r = r.readlines()
+        ref = r[1]
+
     # perform 'chunks' on 'fastq_list' to break the fastq file into chunks of 4 lines
     new = chunks(fastq_list, 4)
 
@@ -45,39 +45,24 @@ if args.VCF :
         vcffile.write(item)
 
     # with open("filename.txt", "wb") as outfile:
-    uniqueList = []
+    uniqueList = [] # creates an empty list in which unique variants will be appended
     for i in range(0, len(new)):
-        a = new[i][1]
+        a = new[i][1] # puts sequence (second list item for each list in new) into variable 'a'
         for j in range(0, len(a)):
-            uniqueStr = "%s,%s" % (str(j), str(a[j]))
-            if a[j] == ref[j]:
+            uniqueStr = "%s,%s" % (str(j), str(a[j])) #creates a string containing the sequence position and read base
+            if a[j] == ref[j]: # pass if sequence at position 'j' is identical
                 pass
-                # print("true" + (a[j]) + ref[j])
-            elif uniqueStr not in uniqueList:
+            elif uniqueStr not in uniqueList: # if sequence at position 'j' is non-identical, and if 'uniqueStr' does not exist in 'uniqueList', add a new line to the VCF file
                 POS = str(j)
-                ID = str(new[i][0])
-                ID = ID.rstrip()  # removes '/n' from ID string
                 REF = a[j]
                 ALT = ref[j]
-                vcffile.write("\n1\t%s\t%s\t%s\t%s\t" % (POS, ID, REF, ALT))
-                uniqueList.append(uniqueStr)
-               # vcffile.write("1\t")
-                #vcffile.write("%s\t" % POS)
-                #vcffile.write("%s\t" % ID)
-                #vcffile.write("%s\t" % REF)
-                #vcffile.write("%s\t" % ALT)
-                # print("false" + a[j] + ref[j])
+                vcffile.write("\n1\t%s\t.\t%s\t%s\t.\t.\t.\t" % (POS, REF, ALT))
+                uniqueList.append(uniqueStr) # adds uniqueStr to uniqueList if string is unique
     vcffile.close()
-    print(uniqueList)
 
-# need to stop variants being added to vcf file if they already exist in vcf file
+    
+    """ Convert Fastq to Fasta file """ # comment about fact that this code already exists on biopython
 
-## \t = tab (use for making vcf file)
-## writing things to file : outfile = open('filename, 'w')
-
-## in vcf file, ID header will be there but will be '.'
-
-""" Convert Fastq to Fasta file """
 else :
     # perform 'chunks' on 'fastq_list' to break the fastq file into chunks of 2 lines, and output to a list 'fqn'
     fqn = chunks(fastq_list, 2)
@@ -85,8 +70,8 @@ else :
     # delete every second chunk of two in fastq_list to remove read quality info
     c = 0
     for i in range(1, len(fqn), 2) :
-        # for each 'chunk' in 'fqn', delete
-        del (fqn[i - c]) # 'c' variable adjusts for list index changes that occur every loop
+        # delete every second chunk of the list 'fqn' (every second chunk contains read quality information)
+        del (fqn[i - c]) # 'c' variable adjusts for list index changes that occur every loop due to deletion of a chunk
         c = c + 1
 
     # extract all items from lists wthin lists of 'fqn' and create list, 'fqn2'
@@ -94,6 +79,11 @@ else :
     for i in range(0, len(fqn)) :
         fqn2.append(fqn[i][0])
         fqn2.append(fqn[i][1])
+
+    # removes beginning '@' character of read ID and prepends '>' to convert to proper fasta file format
+    for i in range(0, len(fqn2), 2):
+        fqn2[i] = fqn2[i][1:]
+        fqn2[i] = ">" + fqn2[i]
 
     # write new fasta file
     fastafile = open(fasta_file, 'w')
